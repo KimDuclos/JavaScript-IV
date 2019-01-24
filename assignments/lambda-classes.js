ecba1806c1b2dbbb9e268a7bc41bdfca9f0c4c5e
Kim Duclos <kimduclos@gmail.com>
2019-01-24 16:46:25 -0700

    Everything working but one undefined on subjects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -23,7 +23,7 @@ class Instructor extends Person {
         console.log(`Today we are learning about ${subject}.`);
     }
     grade(student, subject){
-        console.log(`${student.name} receives a perfect score on ${subject}`);
+        console.log(`${this.name} receives a perfect score on ${subject}`);
     }
 }
 
@@ -35,13 +35,13 @@ class Student extends Person {
         this.faveSubjects = studentAttr.faveSubjects;
     }
     listsSubjects(){
-        console.log(...faveSubjects);
+        console.log(...this.faveSubjects);
     }
     PRAssignment(){
-        console.log(`${student.name} has submitted a PR for ${subject}.`);
+        console.log(`${this.name} has submitted a PR for ${this.subject}.`);
     }
     sprintChalleng(){
-        console.log(`${student.name} has begun spring challeng on ${subject}.`);
+        console.log(`${this.name} has begun spring challenge on ${this.subject}.`);
     }
 }
 
@@ -52,10 +52,10 @@ class ProjectManager extends Instructor {
         this.favInstructor = PMAttr.favInstructor;
     }
     standup(channel){
-        console.log(`${name} announced to ${channel}, @channel standy times!`);
+        console.log(`${this.name} announced to ${channel}, @channel standy times!`);
     }
     debugsCode(name){
-        console.log(`${name} debugs ${student.name}'s code on ${subject}.`)
+        console.log(`${this.name} debugs ${this.name}'s code on ${this.subject}.`)
     }
 }
 
@@ -79,12 +79,14 @@ const Gob = new Person({
   // Test for Instructor 
 
   const Fred = new Instructor({
+    name: "Fred",
     specialty: "Unix",
     favLanguage: "JS",
     catchPhrase: "What you talkin' 'bout Willis",
   });
 
   const Dan = new Instructor({
+    name: "Dan",
     specialty: "Front-End",
     favLanguage: "C++",
     catchPhrase: "Did I catch a niner in there?",
@@ -93,12 +95,14 @@ const Gob = new Person({
 // Test for Student
 
 const Jimbo = new Student({
+    name: "Jimbo",
     previousBackground: "Office Monkey",
     className: "Web17",
     faveSubjects: ["Math", "Programming", "Lunch"],
   });
 
   const Bob = new Student({
+    name: "Bob",
     previousBackground: "Cashier",
     className: "Web18",
     faveSubjects: ["Back-End", "Databases", "React"],
@@ -107,15 +111,41 @@ const Jimbo = new Student({
   // Test for Project Manager
 
   const Jen = new ProjectManager({
+    name: "Jen",
     gradClassName: "Webby17",
     favInstructor: "Professor Awesome"
   });
 
   const Lisa = new ProjectManager({
+    name: "Lisa",
     gradClassName: "Webby18",
     favInstructor: "Me"
   });
 
 
+// Person
 Gob.speak();
 Frank.speak();
+
+// Instructor
+Fred.demo("C#");
+Fred.grade(Gob, "Pascal");
+
+Dan.demo("Basic");
+Dan.grade(Frank, "QBasic");
+
+// Student
+Jimbo.listsSubjects();
+Jimbo.PRAssignment("Arrays");
+Jimbo.sprintChalleng("Less");
+
+Bob.listsSubjects();
+Bob.PRAssignment();
+Bob.sprintChalleng();
+
+// Project Manager
+Jen.standup();
+Jen.debugsCode();
+
+Lisa.standup();
+Lisa.debugsCode();
